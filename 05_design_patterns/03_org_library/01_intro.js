ebf74bffcb2ac01c890ca1b10445eeee869a3820
alexskrn <askrn123@gmail.com>
2020-12-20 17:29:01 +0300

    Added Knockout starter files in 05_design_patterns, lession 03_org_library

---
+++ 05_design_patterns/03_org_library/01_intro.js
@@ -0,0 +1,16 @@
+// MVC - Model View Controller
+// MVVM - Model View ViewModel
+// MVP - Model View Presenter
+
+// jQuery includes AJAX and DOM manipilation libraries, etc. A package
+
+// Fundamental Organizational Concepts
+
+// Models: represent data
+// Collections: smart arrays filled with models
+
+// View Model, or Controller
+
+// Views
+// Routers: keep track of the state of the url. Similar to Views but only for
+// the url bar.
---
+++ 05_design_patterns/03_org_library/02_knockoutJS.js
@@ -0,0 +1,10 @@
+// Model View ViewModel (i.e. controller; seperates Model from View)
+
+// Declarative bindings (formerler, Data <-> Controller <-> DOM; now we put
+// the logic in the DOM in the form of bindings)
+
+// Automatic UI (user interface) Refresh (when data/model changes)
+
+// Dependency tracking - models can depend on other models
+
+// example code: http://jsfiddle.net/rniemeyer/3Lqsx/

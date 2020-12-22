4831ae4314492973d2534394f181e7a213520372
alexskrn <askrn123@gmail.com>
2020-12-22 21:25:36 +0300

    Applied teacher solution to 05_design_patterns, lession 03_org_library, cat-clicker-ko

--- 05_design_patterns/03_org_library/cat-clicker-ko-starter/js/app.js
+++ 05_design_patterns/03_org_library/cat-clicker-ko-starter/js/app.js
@@ -3,10 +3,7 @@ var ViewModel = function() {
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.nicknames = ko.observableArray(
-    [
-      {nickname: 'Tabtab'},
-      {nickname: 'Mr. T'},
-    ]);
+    ['Tabtab', 'Mr. T']);
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);

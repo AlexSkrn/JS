e52256ed61ac253bdee295be794ab9c8fad126b0
alexskrn <askrn123@gmail.com>
2020-12-22 21:21:27 +0300

    Added my solution to 05_design_patterns, lesson 03_org_library, cat-clicker-ko

---
+++ 05_design_patterns/03_org_library/cat-clicker-ko-starter/js/app.js
@@ -0,0 +1,31 @@
+var ViewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+  this.nicknames = ko.observableArray(
+    [
+      {nickname: 'Tabtab'},
+      {nickname: 'Mr. T'},
+    ]);
+
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
+
+  this.level = ko.computed(function() {
+    if (this.clickCount() >= 3 && this.clickCount() < 13) {
+      return 'Infant';
+    }
+    else if (this.clickCount() >= 13 && this.clickCount() < 20) {
+      return 'Teen';
+    }
+    else if (this.clickCount() >= 20) {
+      return 'Old';
+    }
+    else {
+      return 'Newborn';
+    }
+  }, this);
+}
+
+ko.applyBindings(new ViewModel());

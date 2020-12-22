4730dd91ccc50419ff1f933c7f18b70a70a43f21
alexskrn <askrn123@gmail.com>
2020-12-22 22:06:29 +0300

    Refactored solution  05_design_patterns, lession 03_org_library, cat-clicker-ko

--- 05_design_patterns/03_org_library/cat-clicker-ko-starter/js/app.js
+++ 05_design_patterns/03_org_library/cat-clicker-ko-starter/js/app.js
@@ -1,13 +1,9 @@
-var ViewModel = function() {
+var Cat = function(){
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.nicknames = ko.observableArray(
     ['Tabtab', 'Mr. T']);
-
-  this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
-  };
 
   this.level = ko.computed(function() {
     var title;
@@ -26,6 +22,17 @@ var ViewModel = function() {
     }
     return title;
   }, this);
+
+};
+
+var ViewModel = function() {
+
+  this.currentCat = ko.observable(new Cat());
+
+  this.incrementCounter = function() {
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+  };
+
 }
 
 ko.applyBindings(new ViewModel());

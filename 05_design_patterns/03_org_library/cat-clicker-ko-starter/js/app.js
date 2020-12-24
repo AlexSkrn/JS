714690d68fd913b91738fd91ea24acf273ca838c
alexskrn <askrn123@gmail.com>
2020-12-24 19:11:11 +0300

    My solution to task in 05_design_patterns, lession 03_org_lib, cat-clicker-ko

--- 05_design_patterns/03_org_library/cat-clicker-ko-starter/js/app.js
+++ 05_design_patterns/03_org_library/cat-clicker-ko-starter/js/app.js
@@ -1,9 +1,47 @@
-var Cat = function(){
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.nicknames = ko.observableArray(
-    ['Tabtab', 'Mr. T']);
+// Problem
+// - Make all cats show up in the list
+// - Make currentCat change  when I click on a cat in the list
+
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            nicknames: ['Tabtab', 'Mr. T']
+
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            nicknames: ['Tig']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            nicknames: ['Mr. S']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            nicknames: ['Shady']
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            nicknames: ['Nappy']
+        }
+    ];
+
+
+var Cat = function(data){
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.nicknames = ko.observableArray(data.nicknames);
 
   this.level = ko.computed(function() {
     var title;
@@ -23,20 +61,28 @@ var Cat = function(){
     return title;
   }, this);
 
-
-
 };
 
 var ViewModel = function() {
   // alternative solution, using self var to define the ViewModel
   var self = this;
 
-  this.currentCat = ko.observable(new Cat());
+  this.catList = ko.observableArray([]);
 
-  // this.incrementCounter = function() {
-  //   // this represents currentCat's binding context=scope
-  //   this.clickCount(this.clickCount() + 1);
-  // };
+  initialCats.forEach(function(catItem) {
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
+
+  this.showCat = function(clickedCat) {
+    // [ PROBLEM: how to make clickedCat object visible? ]
+    newCatIndex = self.catList.indexOf(clickedCat);
+    // console.log('Cat index in the catList =' + newCatIndex);
+
+    self.currentCat(self.catList()[newCatIndex]);  // this works
+
+  };
 
   this.incrementCounter = function() {
     // self represents the ViewModel
